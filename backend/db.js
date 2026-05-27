45230b8641626376d1f61e95e50cb26da6dfa63d
AssilHalawi <assilhalawi1@gmail.com>
2026-05-27 19:14:13 +0300

    Add files via upload

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});
+
+// Test connection
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("❌ Database connection failed:", err);
+    } else {
+        console.log("✅ Connected to Railway MySQL!");
+        connection.release();
+    }
+});
+
+module.exports = db;
